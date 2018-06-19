12362be16bfab7dab3714701238b8e7dfdeeaf8b
李梦龙 <649076408@qq.com>
2018-06-19 21:43:24 +0800

    chore($page): create site

--- src/veditor/index.js
+++
@@ -1,8 +0,0 @@
-// VEditor 主类
-import VEditor from './editor';
-// VEditor 样式
-import '../styles/editor.scss';
-
-if (typeof window !== 'undefined' && !window.vEditor) {
-  window.vEditor = VEditor;
-}
